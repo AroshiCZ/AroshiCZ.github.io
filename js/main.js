77eb0910e6ff2c4d825e7567488498d05b238506
aroshicz <bohmda22@sps-prosek.cz>
2026-03-24 15:48:02 +0100

    added js config

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
-import "lib.js";
+const title = "";
+const email = "";
+const rel = "";
+const refsheet = "";
+const template_folder = "../templates/";
+const links_icons = {
+    folder: "img/",
+    container : "",
+    template : "icon.html",
+    data : [
+        {
+            link: "",
+            img : "",
+            alt : "",
+            text : "",
+        }
+    ]
+};
+const links_stream = {
+    container : "",
+    template : "stream.html",
+    data : [
+        {
+            name : "",
+            link : "",
+            platform : "",
+        },
+    ]
+};
 
-test();
+const config = {
+    title : title,
+    email : email,
+    rel : rel,
+    refsheet : refsheet,
+    template_folder : template_folder,
+    links_icons : links_icons,
+    links_stream : links_stream,
+}
+
+console.log(config);
